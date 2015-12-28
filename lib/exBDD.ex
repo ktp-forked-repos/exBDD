97883640e9e9891a4f56f47c7bf4fa710f4a8de8
michal j wallace <michal.wallace@gmail.com>
2015-12-28 13:25:45 +0000

    add some comments and cite the paper

--- lib/exBDD.ex
+++ lib/exBDD.ex
@@ -1,6 +1,11 @@
 defmodule ExBDD do
   @doc """
   Binary Decision Diagram module for Elixir.
+  © Copyright 2015 Michal J Wallace <http://tangentstorm.com/>
+  Available for use under the MIT license.
+
+  Based on the 1990 paper, _Efficient Implementation of a BDD Package_,
+  by Karl S. Brace, Richard L. Rudell, and Randal E. Bryant.
   """
   import Bitwise
 
@@ -30,6 +35,7 @@ defmodule ExBDD do
   def ite_norm(f, g, h) do
     nf = bnot f; ng= bnot g; nh= bnot h
     case {f, g, h} do
+      # Perform some standard simplifications:
       {@l, ^g, ^h} -> g
       {@o, ^g, ^h} -> h
       {^f, ^g, ^g} -> g
@@ -42,19 +48,23 @@ defmodule ExBDD do
       {^f, ^g,^nf} -> ite_norm f,  g, @l
       {^f,^nf, ^h} -> ite_norm f, @o,  h
       _ ->
+        # Choose between equivalent forms by putting the smaller variable in the 'if' slot.
         [fv, gv, hv] = for x <- [f, g, h] do get_var x end
         case {g, h} do
-          {@l, ^h} when hv < fv -> ite_norm  h, @l,  f
-          {^g, @o} when gv < fv -> ite_norm  g,  f, @o
-          {^g, @l} when gv < fv -> ite_norm ng, nf, @l
-          {@o, ^h} when hv < fv -> ite_norm nh, @o, nf
-          {^g,^ng} when gv < fv -> ite_norm  g,  f, nf
+          {@l, ^h} when hv < fv -> ite_norm  h, @l,  f  # (f?l:g) = (g?l:f)
+          {^g, @o} when gv < fv -> ite_norm  g,  f, @o  # (f?g:o) = (g?f:o)
+          {^g, @l} when gv < fv -> ite_norm ng, nf, @l  # (f?g:l) = (¬g?¬f:l)
+          {@o, ^h} when hv < fv -> ite_norm nh, @o, nf  # (f?o:h) = (¬h?o:¬f)
+          {^g,^ng} when gv < fv -> ite_norm  g,  f, nf  # (f?g:¬g) = (g?f:¬f)
           _ ->
+            # choose the representation with non-complemented edges in the 'if' and 'then' slots.
+            # 0. (f ? g : h);  1. (¬f ? h : g);  2. ¬(f ? ¬g : ¬h)  3. ¬(¬f ? ¬h : ¬g)
             cond do
               f < 0 -> ite_norm nf, h, g
               g < 0 ->
                 r = ite_norm f, ng, nh
                 if is_integer r do bnot r else %{ not: r } end
+              # that's all we can do. the triple is now fully normalized:
               true -> get_memo f, g, h, {f, g, h}
             end
         end
