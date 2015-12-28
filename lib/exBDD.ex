b334cb06524a3c270e5a650664e5de51019616fb
michal j wallace <michal.wallace@gmail.com>
2015-12-28 16:58:26 +0000

    flesh out implementation, provide N?AND

--- lib/exBDD.ex
+++ lib/exBDD.ex
@@ -9,30 +9,111 @@ defmodule ExBDD do
   """
   import Bitwise
 
+  # node 0 (@o) is the false node. instead of a 'true' node, we
+  # just create a complement edge to the 'false' node. (@l = bnot @o)
   @o 0; @l -1
 
-  @doc """
-  :: nid, nid, nid, default? -> nid | default
-  -- return the memoized nid for (ite f,g,h), if present
-  """
-  def get_memo( _f, _g, _h, default \\ nil ) do
-    default # TODO: actually look up memos
+  @typdoc "node identifier"
+  @type nid :: number
+
+  @typdoc "if/then/else triple"
+  @type bdd :: {nid, nid, nid}
+
+  @typdoc "reference to BDD base"
+  @type base :: pid
+
+  @spec init :: base
+  @doc "create a new BDD base"
+  def init do
+    Agent.start_link fn ->
+      %{ nodes: %{ 0 => { @l, @o, @l } },
+         names: %{ },
+         memos: %{ },
+         next_nid: 1,
+       }
+    end
   end
 
-  @doc """
-  :: nid -> nid
-  -- return the nid for the variable or function on which node n branches
-  """
-  def get_var( n ) do
-    if n < 0 do n = bnot n end
-    if n == @o do @l else n end # TODO: handle more than variables
+  @spec vars(base, [String.t]) :: [nid]
+  @doc "retrieve the nids for the given variables (creating new nodes when necessary)"
+  def vars(base, names) do
+    for v <- names do by_name(base, v) || new_var(base, v) end
   end
 
-  @doc """
-  :: nid, nid, nid → nid | {nid, nid, nid} | %{not: {nid,nid,nid}
-  -- normalize if/then/else triples.
-  """
-  def ite_norm(f, g, h) do
+  @spec by_name(base, String.t) :: nid | nil
+  @doc "retrieve the nids for the given name, if defined"
+  def by_name(base, name) do
+    Agent.get base, fn %{names: names} -> names[name] end
+  end
+
+  @spec new_var(base, String.t) :: nid | nil
+  @doc "create a new node and bind the name to it"
+  def new_var(base, name) do
+    Agent.get_and_update base, fn state ->
+      nid = state[:next_nid]
+      {nid, %{state |
+              nodes: Map.put(state[:nodes], nid, { nid, @l, @o }),
+              names: Map.put(state[:names], name, nid),
+              next_nid: nid+1 }}
+    end
+  end
+
+  @spec node(base, nid) :: bdd
+  @doc "retrieve the if/then/else tuple for the given nid"
+  def node(base, nid) do
+    Agent.get base, fn state ->
+      invert = nid < 0
+      if invert do nid = bnot nid end
+      {f, g, h} = state[:nodes][nid]
+      if invert do {f, (bnot g), (bnot h)} else {f, g, h} end
+    end
+  end
+
+  @spec bAND(base, nid, nid) :: nid
+  @doc "perform a logical AND operation on BDD nids"
+  def bAND(base, a, b), do: ite base, a, b, @o
+
+  @spec bNAND(base, nid, nid) :: nid
+  @doc "perform a logical NAND operation on BDD nids"
+  def bNAND(base, a, b), do: bnot(ite base, a, b, @o)
+
+
+  @spec get_memo(base, nid, nid, nid) :: nid | nil
+  @doc "return the memoized nid for (ite f,g,h), if present"
+  def get_memo(base, f, g, h, default \\ nil ) do
+    Agent.get base, fn state ->
+      Map.get(state[:memos], {f, g, h}, default)
+    end
+  end
+
+  @spec get_nid(base, nid, nid, nid) :: nid
+  @doc "find or create a nid for this triple."
+  def get_nid(base, f, g, h) do
+    if (memo = get_memo base, f, g, h) != nil do memo
+    else
+      Agent.get_and_update base, fn state ->
+        nid = state[:next_nid]
+        {nid, %{state |
+                next_nid: nid + 1,
+                nodes: Map.put(state[:nodes], nid, {f,g,h}),
+                memos: Map.put(state[:memos], {f,g,h}, nid)}}
+      end
+    end
+  end
+
+  @spec get_var(base, nid) :: nid
+  @doc "return the nid for the variable or function on which node n branches"
+  def get_var(base, n) do
+    cond do
+      n < 0 -> bnot (get_var base, (bnot n))
+      n == @o -> @l
+      true -> {f, _g, _h} = (node base, n); f
+    end
+  end
+
+  @spec ite_norm(base, nid, nid, nid) :: nid | bdd | %{not: bdd}
+  @doc "normalize if/then/else triples."
+  def ite_norm(base, f, g, h) do
     nf = bnot f; ng= bnot g; nh= bnot h
     case {f, g, h} do
       # Perform some standard simplifications:
@@ -43,31 +124,80 @@ defmodule ExBDD do
       {^f, @o, @l} -> nf
       {^f, ^f, @o} -> f
       {^f, ^f, @l} -> @l
-      {^f, ^f, ^h} -> ite_norm f, @l,  h
-      {^f, ^g, ^f} -> ite_norm f,  g, @o
-      {^f, ^g,^nf} -> ite_norm f,  g, @l
-      {^f,^nf, ^h} -> ite_norm f, @o,  h
+      {^f, ^f, ^h} -> ite_norm base, f, @l,  h
+      {^f, ^g, ^f} -> ite_norm base, f,  g, @o
+      {^f, ^g,^nf} -> ite_norm base, f,  g, @l
+      {^f,^nf, ^h} -> ite_norm base, f, @o,  h
       _ ->
         # Choose between equivalent forms by putting the smaller variable in the 'if' slot.
-        [fv, gv, hv] = for x <- [f, g, h] do get_var x end
+        [fv, gv, hv] = for x <- [f, g, h] do get_var base, x end
         case {g, h} do
-          {@l, ^h} when hv < fv -> ite_norm  h, @l,  f  # (f?l:g) = (g?l:f)
-          {^g, @o} when gv < fv -> ite_norm  g,  f, @o  # (f?g:o) = (g?f:o)
-          {^g, @l} when gv < fv -> ite_norm ng, nf, @l  # (f?g:l) = (¬g?¬f:l)
-          {@o, ^h} when hv < fv -> ite_norm nh, @o, nf  # (f?o:h) = (¬h?o:¬f)
-          {^g,^ng} when gv < fv -> ite_norm  g,  f, nf  # (f?g:¬g) = (g?f:¬f)
+          {@l, ^h} when hv < fv -> ite_norm base,  h, @l,  f  # (f?l:g) = (g?l:f)
+          {^g, @o} when gv < fv -> ite_norm base,  g,  f, @o  # (f?g:o) = (g?f:o)
+          {^g, @l} when gv < fv -> ite_norm base, ng, nf, @l  # (f?g:l) = (¬g?¬f:l)
+          {@o, ^h} when hv < fv -> ite_norm base, nh, @o, nf  # (f?o:h) = (¬h?o:¬f)
+          {^g,^ng} when gv < fv -> ite_norm base,  g,  f, nf  # (f?g:¬g) = (g?f:¬f)
           _ ->
             # choose the representation with non-complemented edges in the 'if' and 'then' slots.
             # 0. (f ? g : h);  1. (¬f ? h : g);  2. ¬(f ? ¬g : ¬h)  3. ¬(¬f ? ¬h : ¬g)
             cond do
-              f < 0 -> ite_norm nf, h, g
+              f < 0 -> ite_norm base, nf, h, g
               g < 0 ->
-                r = ite_norm f, ng, nh
+                r = ite_norm base, f, ng, nh
                 if is_integer r do bnot r else %{ not: r } end
               # that's all we can do. the triple is now fully normalized:
-              true -> get_memo f, g, h, {f, g, h}
+              true -> {f, g, h}
             end
         end
+    end
+  end
+
+  @spec ite(base, nid, nid, nid) :: nid
+  @doc "if/then/else operation"
+  def ite(base, f, g, h) do
+    case ite_norm base, f, g, h do
+      {f, g, h} -> build_ite base, f, g, h
+      %{not: {f, g, h}} -> bnot (build_ite base, f, g, h)
+      nid when is_integer(nid) -> nid
+    end
+  end
+
+  @spec build_ite(base, nid, nid, nid) :: nid
+  @doc "recursively build a new bdd for the given if/then/else triple"
+  def build_ite(base, f, g, h) do
+    if (res = get_memo base, f, g, h) != nil do res
+    else
+      vars = for n <- [f,g,h] do get_var base, n end
+      if_ = Enum.min(for v <- vars, v > 0, do: v)
+      [f1, g1, h1] = for n <- [f,g,h] do whenHi(base, if_, n) end
+      [f0, g0, h0] = for n <- [f,g,h] do whenLo(base, if_, n) end
+      th_ = ite base, f1, g1, h1
+      el_ = ite base, f0, g0, h0
+      if th_ == el_ do th_ else get_nid base, if_, th_, el_ end
+    end
+  end
+
+  @spec whenHi(base, nid, nid) :: nid
+  @doc "return the value of the node when var is true"
+  def whenHi(base, var, nid) do
+    {v, hi, lo} = node base, nid
+    cond do
+      v == @l -> nid
+      var == v  -> hi
+      var <  v  -> nid
+      var >  v  -> ite base, v, (whenHi base, var, hi), (whenHi base, var, lo)
+    end
+  end
+
+  @spec whenHi(base, nid, nid) :: nid
+  @doc "return the value of the node when var is false"
+  def whenLo(base, var, nid) do
+    {v, hi, lo} = node base, nid
+    cond do
+      v == @l -> nid
+      var == v  -> lo
+      var <  v  -> nid
+      var >  v  -> ite base, v, (whenLo base, var, hi), (whenLo base, var, lo)
     end
   end
 
--- test/exBDD_test.exs
+++ test/exBDD_test.exs
@@ -1,8 +1,47 @@
 defmodule ExBDDTest do
   use ExUnit.Case
   doctest ExBDD
+  import Bitwise
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @o 0; @l -1
+
+  setup do
+    {:ok, base} = ExBDD.init
+    {:ok, base: base}
   end
+  
+  test "variables", %{base: base} do
+    [a, b] = ExBDD.vars base, ["a", "b"]
+    assert [a, b] == [1, 2]
+    [na, nb] = [(bnot a), (bnot b)]
+    assert (ExBDD.node base, a) == { a, @l, @o }
+    assert (ExBDD.node base, b) == { b, @l, @o }
+    assert (ExBDD.node base, na) == { a, @o, @l }
+    assert (ExBDD.node base, nb) == { b, @o, @l }
+  end
+
+
+  test "bAND", %{base: base} do
+    [a, b] = ExBDD.vars base, ["a", "b"]
+    f = ExBDD.bAND base, a, b
+    g = ExBDD.bAND base, b, a
+    assert f == g
+    assert (ExBDD.node base, f) == { a, b, @o }
+    assert (ExBDD.whenHi base, a, f) == b
+    assert (ExBDD.whenLo base, a, f) == @o
+    assert (ExBDD.whenHi base, b, f) == a
+    assert (ExBDD.whenLo base, b, f) == @o
+  end
+
+  test "bNAND", %{base: base} do
+    [a, b] = ExBDD.vars base, ["a", "b"]
+    nf = ExBDD.bAND base, a, b
+    f = ExBDD.bNAND base, a, b
+    assert f == bnot nf
+    assert (ExBDD.whenLo base, a, f) == @l
+    assert (ExBDD.whenHi base, a, f) == bnot b
+    assert (ExBDD.whenLo base, b, f) == @l
+    assert (ExBDD.whenHi base, b, f) == bnot a
+  end
+
 end
