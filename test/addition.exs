7392d2219d4143e02e063bbe69e6da5ced262243
tangentstorm <michal.wallace@gmail.com>
2015-12-29 19:48:14 -0500

    move addition test to its own script

---
+++ test/addition.exs
@@ -0,0 +1,24 @@
+import ExBDD.Ints
+
+{:ok, base} = ExBDD.init
+[a,b,c,d,e,f,g,h] = (make_ints base, ["a","b","c","d","e","f","g","h"], n=32)
+
+IO.puts "------- a + b --------------"
+sumAB = add base, a, b
+IO.puts "------- c + d --------------"
+sumCD = add base, c, d
+
+IO.puts "------- ab + cd ------------"
+sumABCD = add base, sumAB, sumCD
+
+IO.puts "------- e + f --------------"
+sumEF = add base, e, f
+IO.puts "------- g + h --------------"
+sumGH = add base, g, h
+
+
+IO.puts "------- ef + gh ------------"
+sumEFGH = add base, sumEF, sumGH
+
+IO.puts "----- abcd + efgh ----------"
+sumABCDEFGH = add base, sumABCD, sumEFGH
--- test/exBDD_ints_test.exs
+++ test/exBDD_ints_test.exs
@@ -8,36 +8,4 @@ defmodule ExBDDIntsTest do
     assert [[1,4,7],[2,5,8],[3,6,9]] == (make_ints base, ["a","b","c"], 3)
   end
 
-  @tag timeout: 800000000
-  test "add" do
-    {:ok, base} = ExBDD.init
-    [a,b,c,d,e,f,g,h] = (make_ints base, ["a","b","c","d","e","f","g","h"], n=32)
-    IO.puts "------- a + b --------------"
-    sumAB = add base, a, b
-    IO.puts "------- c + d --------------"
-    sumCD = add base, c, d
-    assert (Enum.count sumAB) == n
-    assert (Enum.count sumCD) == n
-
-		IO.puts "------- ab + cd ------------"
-    sumABCD = add base, sumAB, sumCD
-    assert (Enum.count sumABCD) == n
-
-		IO.puts "------- e + f --------------"
-    sumEF = add base, e, f
-    IO.puts "------- g + h --------------"
-    sumGH = add base, g, h
-    assert (Enum.count sumEF) == n
-    assert (Enum.count sumGH) == n
-
-		IO.puts "------- ef + gh ------------"
-    sumEFGH = add base, sumEF, sumGH
-    assert (Enum.count sumEFGH) == n
-
-		IO.puts "----- abcd + efgh ----------"
-    sumABCDEFGH = add base, sumABCD, sumEFGH
-    assert (Enum.count sumABCDEFGH) == n
-  end
-
-
 end
